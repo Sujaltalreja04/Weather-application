ed67c26a3fd90d04ce6293b0449e5efa492cdc42
Sujaltalreja04 <128150608+Sujaltalreja04@users.noreply.github.com>
2024-07-06 19:17:52 +0530

    file added

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{2f745fa85d563da5adb87b6cd4b81caf}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
